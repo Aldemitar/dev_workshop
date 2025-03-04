7ed5611a95910f7502cc4cb559935e41fdb31f75
Aldemitar <aldemarcatanocardona@gmail.com>
2025-03-03 19:54:01 -0500

    Se realizó la función de merge_ordenado y quedo aprobada

--- tests/test_data.py
+++ tests/test_data.py
@@ -19,7 +19,24 @@ class Data:
                 resultado.append(elemento)
                 vistos.add((elemento, type(elemento)))
         return resultado
-
+    
+    def merge_ordenado(self, lista1, lista2):
+        resultado = []
+        i, j = 0, 0  
+        while i < len(lista1) and j < len(lista2):
+            if lista1[i] < lista2[j]:
+                resultado.append(lista1[i])
+                i += 1
+            else:
+                resultado.append(lista2[j])
+                j += 1
+        while i < len(lista1):
+            resultado.append(lista1[i])
+            i += 1
+        while j < len(lista2):
+            resultado.append(lista2[j])
+            j += 1
+        return resultado
 
 class TestData:
     def setup_method(self):
