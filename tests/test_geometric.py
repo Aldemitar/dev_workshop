6b9f68e860463ec91643f572ffa273967065ad0c
Aldemitar <aldemarcatanocardona@gmail.com>
2025-03-03 21:03:03 -0500

    Se realizó la función de area_trapecio y quedo aprobada

--- tests/test_geometric.py
+++ tests/test_geometric.py
@@ -24,6 +24,9 @@ class Geometria:
     def es_triangulo_valido(self, lado1, lado2, lado3):
         return lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1 and lado1 > 0 and lado2 > 0 and lado3 > 0
 
+    def area_trapecio(self, base_mayor, base_menor, altura):
+        return 0.5 * (base_mayor + base_menor) * altura
+
 
 class TestGeometria:
     def setup_method(self):
