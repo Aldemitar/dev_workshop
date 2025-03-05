91d4beabec2bdd5e5cb808aaca7f8217bdd5fe5f
Aldemitar <aldemarcatanocardona@gmail.com>
2025-03-05 15:40:41 -0500

    Se agrego el contenido de la función es_cuadrado_magico para el test de magic

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -76,13 +76,13 @@ class Magic:
         return n == sum(int(digit) ** a for digit in num_str)
     
     def es_cuadrado_magico(self, matriz):
-        """
-        Verifica si una matriz es un cuadrado mágico (suma igual en filas, columnas y diagonales).
-        
-        Args:
-            matriz (list): Lista de listas que representa una matriz cuadrada
-            
-        Returns:
-            bool: True si es un cuadrado mágico, False en caso contrario
-        """
-        pass
+        n = len(matriz)
+        suma_filas = [sum(fila) for fila in matriz]
+        suma_columnas = [sum(matriz[i][j] for i in range(n)) for j in range(n)]
+        suma_diagonal_principal = sum(matriz[i][i] for i in range(n))
+        suma_diagonal_secundaria = sum(matriz[i][n - 1 - i] for i in range(n))
+        return (
+            len(set(suma_filas)) == 1 and
+            len(set(suma_columnas)) == 1 and
+            suma_filas[0] == suma_columnas[0] == suma_diagonal_principal == suma_diagonal_secundaria
+        )
